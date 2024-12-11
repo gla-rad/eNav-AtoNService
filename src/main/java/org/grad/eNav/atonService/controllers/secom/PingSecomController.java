130f5fd8867a2d638444c4b4b902f88f7d7013f2
Lawrence Hughes <lawrence.hughes@gla-rad.org>
2024-12-11 16:09:13 +0000

    Added copyright to Ping interface

--- src/main/java/org/grad/eNav/atonService/controllers/secom/PingSecomController.java
+++ src/main/java/org/grad/eNav/atonService/controllers/secom/PingSecomController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2024 GLA Research and Development Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.grad.eNav.atonService.controllers.secom;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,7 +25,11 @@ import org.springframework.validation.annotation.Validated;
 
 import java.time.Instant;
 
-
+/**
+ * The SECOM Ping Interface Controller.
+ *
+ * @author Lawrence Hughes (email: Lawrence.Hughes@gla-rad.org)
+ */
 @Component
 @Path("/")
 @Validated
