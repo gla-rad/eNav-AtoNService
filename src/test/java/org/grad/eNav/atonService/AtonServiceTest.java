1932052fcb451e72794b4ec4945c1dda3bdccef5
Nikolaos Vastardis <Nikolaos.Vastardis@gla-rad.org>
2025-07-09 09:31:31 +0100

    Fixing the contextLoads unit test

--- src/test/java/org/grad/eNav/atonService/AtonServiceTest.java
+++ src/test/java/org/grad/eNav/atonService/AtonServiceTest.java
@@ -17,13 +17,19 @@
 package org.grad.eNav.atonService;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 @TestPropertySource("classpath:application.properties")
-@Import(TestingConfiguration.class)
+@Import({TestingConfiguration.class, TestFeignSecurityConfig.class})
 class AtonServiceTest {
 
 	@Test
