9b248745033af3e362c9d226b3efe9bd4a97bfc3
Nikolaos Vastardis <Nikolaos.Vastardis@gla-rad.org>
2025-08-14 16:21:31 +0100

    Removing zookeeper connection

--- src/main/java/org/grad/eNav/atonService/config/GSDataStoreConfig.java
+++ src/main/java/org/grad/eNav/atonService/config/GSDataStoreConfig.java
@@ -69,7 +69,6 @@ public class GSDataStoreConfig {
         // The the connection parameters
         Map<String, String> params = new HashMap<>();
         params.put("kafka.brokers", kafkaBrokers);
-        params.put("kafka.zookeepers", kafkaZookeepers);
         params.put("kafka.consumer.config", String.format("%s=%d", ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 10485760));
         params.put("kafka.consumer.count", Objects.toString(noKafkaConsumers));
 
